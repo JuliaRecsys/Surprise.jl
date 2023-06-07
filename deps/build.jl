57fa471fbd28738e4b0da0128b36f320ddc318a2
Filipe Braida <filipebraida@gmail.com>
2023-06-07 11:22:34 -0300

    build: update packages

--- deps/build.jl
+++ deps/build.jl
@@ -6,9 +6,15 @@ function installpypackage()
 		pyimport_conda("sklearn", "scikit-surprise")
 	catch
 		try
-			Conda.add("scikit-surprise", channel="conda-forge")
+			Conda.add("scikit-learn")
 		catch
 			println("scikit-learn failed to install")
+		end
+
+		try
+			Conda.add("scikit-surprise", channel="conda-forge")
+		catch
+			println("scikit-surprise failed to install")
 		end
 	end
 end
